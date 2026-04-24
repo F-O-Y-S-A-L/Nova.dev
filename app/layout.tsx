ad21c295c53da6b69a032777fea97032ef481702
Foysal <foysal@example.com>
2026-04-24 22:32:17 +0600

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,30 +2,30 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Foysal-three - Fullstack Portfolio",
+  title: "Foysal-devs - Fullstack Portfolio",
   description:
     "A high-performance technical portfolio for a Fullstack Web Developer, featuring modern design and interactive showcases.",
   icons: {
     icon: "/coding-web-development-svgrepo-com.svg",
   },
   openGraph: {
-    title: "Foysal-three - Fullstack Portfolio",
+    title: "Foysal-devs - Fullstack Portfolio",
     description:
       "A high-performance technical portfolio for a Fullstack Web Developer, featuring modern design and interactive showcases.",
-    url: "https://Foysal-three.vercel.app",
-    siteName: "Foysal-three",
+    url: "https://foysal-devs.vercel.app/",
+    siteName: "Foysal-devs",
     images: [
       {
         url: "/title.jpeg",
         width: 1200,
         height: 630,
-        alt: "Foysal-three Title Image",
+        alt: "Foysal-devs Title Image",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "Foysal-three - Fullstack Portfolio",
+    title: "Foysal-devs - Fullstack Portfolio",
     description:
       "A high-performance technical portfolio for a Fullstack Web Developer, featuring modern design and interactive showcases.",
     images: ["/title.jpg"],
